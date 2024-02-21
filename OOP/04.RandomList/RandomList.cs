6e3757d832f41139780008aa96b3ce489dd1506d
lilindra <143004196+lilindra@users.noreply.github.com>
2024-02-21 12:03:19 +0200

    adv

---
+++ OOP/04.RandomList/RandomList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        public string RandomString()
+        {
+            Random random = new Random();
+
+            if (this.Count > 0)
+            {
+                int randomIndex = random.Next(0, this.Count);
+                string randomElement = this[randomIndex];
+                this.RemoveAt(randomIndex);
+                return randomElement;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ OOP/05.StackofStrings/StackOfStrings.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            if (this.Count == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void AddRange(IEnumerable<string> collection)
+        {
+            foreach (string item in collection)
+            this.Push(item);
+        }
+    }
+}
